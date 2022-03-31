6770c1646920fe1419e5f4f559784ac0952959e4
Edwin Melara <edwin.melara.landaverde@gmail.com>
2022-03-31 16:28:41 -0600

    Arreglos

--- app/Models/Usuario_perfil.php
+++ app/Models/Usuario_perfil.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Usuario_perfil extends Model
 {
-    use HasFactory;
-    protected $fillable = ['usuario_id', 'perfil_id', 'estado'];
+  use HasFactory;
+  protected $fillable = ['usuario_id', 'perfil_id', 'estado'];
 }
