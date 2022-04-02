d3d6898eab2ea7fb91fc942c70e41e38b3832e81
Edwin Melara <edwin.melara.landaverde@gmail.com>
2022-04-01 23:38:27 -0600

    Remove Grapqhl

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,30 +1,27 @@
 <?php
 
 namespace App\Providers;
-use Laravel\Passport\Passport;
+
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
-    ];
+  /**
+   * The policy mappings for the application.
+   *
+   * @var array<class-string, class-string>
+   */
+  protected $policies = [
+    // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+  ];
 
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->registerPolicies();
-         Passport::routes();
-        //
-    }
+  /**
+   * Register any authentication / authorization services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    $this->registerPolicies();
+  }
 }
